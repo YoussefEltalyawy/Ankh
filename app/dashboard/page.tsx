0317774c4bf9f5de7a8e52a312ce74a01a10f25b
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2024-09-17 18:23:03 +0300

    users are now saved in supabase

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,54 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 import DashboardClient from "./DashboardClient";
+import prisma from "../lib/db";
+
+type UserData = {
+  email: string | undefined | null;
+  id: string;
+  name: string | undefined | null;
+  pfp: string | undefined | null;
+}
+
+async function createOrFindUser(userData: UserData): Promise<void> {
+  const { id, email, name, pfp } = userData;
+
+  const existingUser = await prisma.user.findUnique({
+    where: { id },
+    select: { id: true },
+  });
+
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        id,
+        email: email || undefined, // This will set email to undefined if it's null or an empty string
+        name,
+        pfp,
+      },
+    });
+  }
+}
+
 async function DashboardPage() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
-  return <DashboardClient user={user}/>
+
+  if (!user) {
+    return redirect("/");
+  }
+
+  console.log(user.id);
+  console.log("up");
+
+  await createOrFindUser({
+    email: user.email as string,
+    id: user.id as string,
+    name: user.given_name as string,
+    pfp: user.picture as string,
+  });
+
+  return <DashboardClient user={user} />;
 }
+
 export default DashboardPage;
