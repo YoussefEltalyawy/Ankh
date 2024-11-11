8a7c39c3d22f8afc1efe48af2265185d5b05232b
Youssef Eltalyawy <yousefeltalyawy@gmail.com>
2024-11-12 00:14:20 +0200

    minor ui fix in mobile landing page

--- app/components/LandHeader.tsx
+++ app/components/LandHeader.tsx
@@ -1,54 +1,62 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Header = () => {
+const HeaderCentered = () => {
   return (
-    <div className="mb-[88px]">
-      <header className="bg-[#F3F3F3] text-black mx-4 md:mx-[160px] pt-[54px]">
-        <div className="container flex items-center justify-between">
-          <div>
-            <Link href="/">
-              <h2 className="text-h2 font-manrope font-bold">Ankh</h2>
-            </Link>
-          </div>
-          {/* Hide on mobile, show on medium screens and up */}
-          <div className="hidden md:block">
-            <ul className="font-manrope text-p flex w-full gap-[4rem]">
-              <li>
-                <Link href="#features">Features</Link>
-              </li>
-              <li>
-                <Link href="/about">About Us</Link>
-              </li>
-              <li>
-                <Link href="/pricing">Contact</Link>
-              </li>
-            </ul>
-          </div>
-          {/* Hide on mobile, show on medium screens and up */}
-          <div className="hidden md:block">
-            <ul className="font-manrope flex items-center">
-              <Link href={"https://github.com/YoussefEltalyawy/Ankh"}>
+    <div className="mb-20">
+      <header className="bg-[#F3F3F3] text-black">
+        {/* Mobile Header - Centered */}
+        <div className="md:hidden flex justify-center py-6">
+          <Link href="/">
+            <h2 className="text-2xl font-manrope font-bold">Ankh</h2>
+          </Link>
+        </div>
+
+        {/* Desktop Header */}
+        <div className="hidden md:block mx-[160px] pt-[54px]">
+          <div className="container flex items-center justify-between">
+            <div>
+              <Link href="/">
+                <h2 className="text-h2 font-manrope font-bold">Ankh</h2>
+              </Link>
+            </div>
+            <div>
+              <ul className="font-manrope text-p flex w-full gap-16">
+                <li>
+                  <Link href="#features">Features</Link>
+                </li>
+                <li>
+                  <Link href="/about">About Us</Link>
+                </li>
+                <li>
+                  <Link href="/pricing">Contact</Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul className="font-manrope flex items-center">
+                <Link href="https://github.com/YoussefEltalyawy/Ankh">
+                  <li>
+                    <Image
+                      src="github.svg"
+                      alt="github"
+                      className="mr-8 w-[38px] h-[38px]"
+                      width={38}
+                      height={38}
+                    />
+                  </li>
+                </Link>
                 <li>
                   <Image
-                    src="github.svg"
-                    alt="github"
-                    className="mr-[2rem] w-[38px] h-[38px]"
+                    className="mr-8 w-[38px] h-[38px]"
+                    src="discord.svg"
+                    alt="discord-logo"
                     width={38}
                     height={38}
                   />
                 </li>
-              </Link>
-              <li>
-                <Image
-                  className="mr-[2rem] w-[38px] h-[38px]"
-                  src="discord.svg"
-                  alt="discord-logo"
-                  width={38}
-                  height={38}
-                />
-              </li>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
@@ -56,4 +64,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default HeaderCentered;
